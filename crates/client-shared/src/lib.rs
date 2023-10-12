512c9ed0394b90a065038fb3873f66acb8d3ffa5
Wolfgang Silbermayr <w.silbermayr@opentalk.eu>
2023-10-12 15:10:16 +0200

    create scaffolding for opentalk-client-shared crate

---
+++ crates/client-shared/src/lib.rs
@@ -0,0 +1,22 @@
+// SPDX-FileCopyrightText: OpenTalk GmbH <mail@opentalk.eu>
+//
+// SPDX-License-Identifier: EUPL-1.2
+
+//! Types and traits that are used by the OpenTalk client library crate
+
+#![warn(
+    bad_style,
+    missing_debug_implementations,
+    missing_docs,
+    overflowing_literals,
+    patterns_in_fns_without_body,
+    trivial_casts,
+    trivial_numeric_casts,
+    unsafe_code,
+    unused,
+    unused_extern_crates,
+    unused_import_braces,
+    unused_qualifications,
+    unused_results,
+    clippy::pedantic
+)]
