314742b180884b81d4e139d3e9b0ba0f79dead61
Wolfgang Silbermayr <w.silbermayr@opentalk.eu>
2023-10-12 15:26:03 +0200

    client-shared: add Method enum and Request trait

--- crates/client-shared/src/lib.rs
+++ crates/client-shared/src/lib.rs
@@ -20,3 +20,36 @@
     unused_results,
     clippy::pedantic
 )]
+
+use serde::{de::DeserializeOwned, Serialize};
+
+/// Access methods used by client for accessing the API
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum Method {
+    /// HTTP GET method
+    GET,
+
+    /// HTTP POST method
+    POST,
+
+    /// HTTP PUT method
+    PUT,
+
+    /// HTTP PATCH method
+    PATCH,
+
+    /// HTTP DELETE method
+    DELETE,
+}
+
+/// A trait implemented for types that are sent to the API as parameters
+pub trait Request: std::fmt::Debug + Serialize {
+    /// The response type that is expected to the request
+    type Response: DeserializeOwned;
+
+    /// The API endpoint path relative to the base URL
+    const PATH: &'static str;
+
+    /// The method used to send the data to the API endpoint
+    const METHOD: Method;
+}
