4c49e84edb9102e342e5e5b159313b3803fb320d
Wolfgang Silbermayr <w.silbermayr@opentalk.eu>
2023-11-16 06:50:17 +0100

    types: add some derives to ParticipantId
    Closes #444

--- crates/types/src/core/participant_id.rs
+++ crates/types/src/core/participant_id.rs
@@ -2,8 +2,7 @@
 //
 // SPDX-License-Identifier: EUPL-1.2
 
-#[cfg(feature = "redis")]
-use redis::RedisError;
+use derive_more::{Display, From, FromStr, Into};
 use uuid::Uuid;
 
 #[allow(unused_imports)]
@@ -13,16 +12,17 @@ use crate::imports::*;
 ///
 /// Generated as soon as the user connects to the websocket and authenticated himself,
 /// it is used to store all participant related data and relations.
-#[derive(Debug, Copy, Clone, Ord, PartialOrd, Eq, PartialEq, Hash)]
+#[derive(
+    Debug, Copy, Clone, Ord, PartialOrd, Eq, PartialEq, Hash, Display, Into, From, FromStr,
+)]
 #[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
-#[cfg_attr(feature = "redis", derive(ToRedisArgs), to_redis_args(fmt))]
+#[cfg_attr(
+    feature = "redis",
+    derive(FromRedisValue, ToRedisArgs),
+    from_redis_value(FromStr),
+    to_redis_args(fmt)
+)]
 pub struct ParticipantId(Uuid);
-
-impl From<Uuid> for ParticipantId {
-    fn from(value: Uuid) -> Self {
-        Self(value)
-    }
-}
 
 impl ParticipantId {
     /// Create a ZERO participant id, e.g. for testing purposes
@@ -39,32 +39,5 @@ impl ParticipantId {
     #[cfg(feature = "rand")]
     pub fn generate() -> Self {
         Self(Uuid::new_v4())
-    }
-}
-
-impl std::fmt::Display for ParticipantId {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        self.0.fmt(f)
-    }
-}
-
-#[cfg(feature = "redis")]
-impl FromRedisValue for ParticipantId {
-    fn from_redis_value(v: &redis::Value) -> RedisResult<Self> {
-        use std::str::{from_utf8, FromStr as _};
-        match v {
-            redis::Value::Data(bytes) => Uuid::from_str(from_utf8(bytes)?)
-                .map(ParticipantId)
-                .map_err(|_| {
-                    RedisError::from((
-                        redis::ErrorKind::TypeError,
-                        "invalid data for ParticipantId",
-                    ))
-                }),
-            _ => RedisResult::Err(RedisError::from((
-                redis::ErrorKind::TypeError,
-                "invalid data type for ParticipantId",
-            ))),
-        }
     }
 }
