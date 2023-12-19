a2dd815f7b5e63a0f86481ef40148e6a8744217e
Maximilian Fuß <m.fuss@opentalk.eu>
2023-12-19 11:35:58 +0100

    use proc-macro-fields-helper in to_redis_args

--- crates/redis-args-impl/src/to_redis_args.rs
+++ crates/redis-args-impl/src/to_redis_args.rs
@@ -4,7 +4,10 @@
 
 use proc_macro::TokenStream;
 use proc_macro2::Span;
+use proc_macro_fields_helper::{get_fields, get_format_macro_call, Fields};
 use quote::quote;
+
+const ATTRIBUTE_NAME: &str = "to_redis_args";
 
 pub(crate) fn to_redis_args(input: TokenStream) -> TokenStream {
     let ast = syn::parse_macro_input!(input as syn::DeriveInput);
@@ -32,11 +35,11 @@ fn get_to_redis_args_conversion(
     let mut found_attr = None;
     for attr in attrs {
         if let Some(segment) = attr.path().segments.iter().next() {
-            if segment.ident == "to_redis_args" {
+            if segment.ident == ATTRIBUTE_NAME {
                 if found_attr.is_some() {
                     return Err(syn::Error::new(
                         Span::call_site(),
-                        "Multiple #[to_redis_args(...)] found",
+                        format!("Multiple #[{ATTRIBUTE_NAME}(...)] found",),
                     ));
                 } else {
                     found_attr = Some(attr);
@@ -51,7 +54,7 @@ fn get_to_redis_args_conversion(
 
     Err(syn::Error::new(
         Span::call_site(),
-        "Attribute #[to_redis_args(...)] missing for #[derive(ToRedisArgs)]",
+        format!("Attribute #[{ATTRIBUTE_NAME}(...)] missing for #[derive(ToRedisArgs)]"),
     ))
 }
 
@@ -63,17 +66,11 @@ enum ToRedisArgsConversion {
     Display,
 }
 
-enum Fields {
-    Named(Vec<syn::Ident>),
-    Unnamed(usize),
-    Empty,
-}
-
 fn parse_to_redis_args_attribute_meta(
     meta: syn::Meta,
 ) -> Result<ToRedisArgsConversion, syn::Error> {
     fn create_generic_error_message() -> syn::Error {
-        syn::Error::new(Span::call_site(), "Attribute #[to_redis_args(...)] requires either `fmt`, `fmt = \"...\"`, `serde`, or `Display`  parameter")
+        syn::Error::new(Span::call_site(), format!("Attribute #[{ATTRIBUTE_NAME}(...)] requires either `fmt`, `fmt = \"...\"`, `serde`, or `Display`  parameter"))
     }
 
     match meta {
@@ -85,7 +82,7 @@ fn parse_to_redis_args_attribute_meta(
             if !matches!(delimiter, syn::MacroDelimiter::Paren(_)) {
                 return Err(syn::Error::new(
                     Span::call_site(),
-                    "Attribute #[to_redis_args(...)] must have parentheses: '('",
+                    format!("Attribute #[{ATTRIBUTE_NAME}(...)] must have parentheses: '('"),
                 ));
             }
 
@@ -107,12 +104,12 @@ fn parse_to_redis_args_attribute_meta(
                 Some(proc_macro2::TokenTree::Punct(punct)) if punct.as_char() == '=' => {
                     if conversion == ToRedisArgsConversion::Serde {
                         return Err(syn::Error::new(Span::call_site(),
-                            "Attribute #[to_redis_args(serde)] does not allow additional parameters"
+                            format!("Attribute #[{ATTRIBUTE_NAME}(serde)] does not allow additional parameters")
                         ));
                     }
 
                     let tokens = proc_macro2::TokenStream::from_iter(tokens);
-                    let s = syn::parse2::<syn::LitStr>(tokens).unwrap();
+                    let s = syn::parse2::<syn::LitStr>(tokens)?;
                     Ok(ToRedisArgsConversion::Format(s.value()))
                 }
                 Some(_) => Err(syn::Error::new(Span::call_site(), "Unexpected token")),
@@ -122,22 +119,8 @@ fn parse_to_redis_args_attribute_meta(
         syn::Meta::Path(_) => Err(create_generic_error_message()),
         syn::Meta::NameValue(_) => Err(syn::Error::new(
             Span::call_site(),
-            "Attribute #[from_redis_value(...)] does not allow assignments inside the parentheses",
+            format!("Attribute #[{ATTRIBUTE_NAME}(...)] does not allow assignments inside the parentheses"),
         )),
-    }
-}
-
-fn get_fields(fields: &syn::Fields) -> Fields {
-    match &fields {
-        syn::Fields::Named(fields) => Fields::Named(
-            fields
-                .named
-                .iter()
-                .filter_map(|field| field.ident.as_ref().cloned())
-                .collect(),
-        ),
-        syn::Fields::Unnamed(fields) => Fields::Unnamed(fields.unnamed.len()),
-        syn::Fields::Unit => Fields::Empty,
     }
 }
 
@@ -148,70 +131,30 @@ fn impl_to_redis_args_fmt(input: &syn::DeriveInput, fmt: &str) -> Result<TokenSt
         syn::Data::Struct(syn::DataStruct { fields, .. }) => {
             let fields = get_fields(fields);
 
-            match fields {
-                Fields::Named(fields) => {
-                    let field_args = fields.iter().filter_map(|field| {
-                        if fmt.contains(&format!("{{{field}}}")) {
-                            Some(quote! {
-                                #field=self.#field
-                            })
-                        } else {
-                            None
-                        }
-                    });
-                    let expanded = quote! {
-                        impl #generics ::redis_args::__exports::redis::ToRedisArgs for #ident #generics {
-                            fn write_redis_args<W>(&self, out: &mut W)
-                            where
-                                W: ?Sized + ::redis_args::__exports::redis::RedisWrite,
-                            {
-                                out.write_arg(format!(#fmt, #(#field_args),*).as_bytes())
-                            }
-                        }
-                    };
-                    Ok(TokenStream::from(expanded))
-                }
-                Fields::Unnamed(count) => {
-                    // A very naive and probably fragile way to get the number of arguments in the format string.
-                    // Should work for most cases, but could be improved someday.
-                    let num_arguments =
-                        fmt.replace("{{", "").replace("}}", "").matches('{').count();
-
-                    if num_arguments > count {
-                        return Err(syn::Error::new(
-                            Span::call_site(),
-                            "Too many arguments in #[redis_args] format string.",
-                        ));
-                    }
-
-                    let field_args = (0..num_arguments).map(|i| {
-                        let index = syn::Index::from(i);
-                        quote! {
-                            self.#index
-                        }
-                    });
-
-                    let expanded = quote! {
-                        impl #generics ::redis_args::__exports::redis::ToRedisArgs for #ident #generics {
-                            fn write_redis_args<W>(&self, out: &mut W)
-                            where
-                                W: ?Sized + ::redis_args::__exports::redis::RedisWrite,
-                            {
-                                out.write_arg(format!(#fmt, #(#field_args),*).as_bytes())
-                            }
-                        }
-                    };
-                    Ok(TokenStream::from(expanded))
-                }
-                Fields::Empty => Err(syn::Error::new(
+            if matches!(fields, Fields::Empty) {
+                return Err(syn::Error::new(
                     Span::call_site(),
-                    "The #[redis_args] attribute can only be attached to structs with fields.",
-                )),
+                    format!("The #[{ATTRIBUTE_NAME}] attribute can only be attached to structs with fields."),
+                ));
             }
+
+            let format_macro_call = get_format_macro_call(ATTRIBUTE_NAME, fmt, &fields)?;
+
+            let expanded = quote! {
+                impl #generics ::redis_args::__exports::redis::ToRedisArgs for #ident #generics {
+                    fn write_redis_args<W>(&self, out: &mut W)
+                    where
+                        W: ?Sized + ::redis_args::__exports::redis::RedisWrite,
+                    {
+                        out.write_arg(#format_macro_call.as_bytes())
+                    }
+                }
+            };
+            Ok(TokenStream::from(expanded))
         }
         syn::Data::Enum(_) | syn::Data::Union(_) => Err(syn::Error::new(
             Span::call_site(),
-            "#[to_redis_args(fmt)] can only be used with structs",
+            format!("#[{ATTRIBUTE_NAME}(fmt)] can only be used with structs"),
         )),
     }
 }
