bbd9ffb2f065344a4911563062d2f0676c9e3b20
Wolfgang Silbermayr <w.silbermayr@opentalk.eu>
2024-01-25 08:45:35 +0100

    refactor(types): move GetRoomEventResponse into separate module

--- crates/types/src/api/v1/rooms.rs
+++ crates/types/src/api/v1/rooms.rs
@@ -4,12 +4,8 @@
 
 //! This module contains types that are used for OpenTalk API V1 rooms endpoints.
 
-use crate::common::event::EventInfo;
-
-#[allow(unused_imports)]
-use crate::imports::*;
-
 mod get_room_event_request;
+mod get_room_event_response;
 mod patch_rooms_request_body;
 mod post_rooms_request_body;
 mod post_rooms_start_invited_request_body;
@@ -22,6 +18,7 @@ pub mod sip_config_resource;
 pub mod streaming_targets;
 
 pub use get_room_event_request::GetRoomEventRequest;
+pub use get_room_event_response::GetRoomEventResponse;
 pub use patch_rooms_request_body::PatchRoomsRequestBody;
 pub use post_rooms_request_body::PostRoomsRequestBody;
 pub use post_rooms_start_invited_request_body::PostRoomsStartInvitedRequestBody;
@@ -29,8 +26,3 @@ pub use post_rooms_start_request_body::PostRoomsStartRequestBody;
 pub use room_resource::RoomResource;
 pub use rooms_start_response::RoomsStartResponse;
 pub use start_room_error::StartRoomError;
-
-/// The JSON body returned by the `/rooms/<room_id>/event` endpoint
-#[derive(Clone, Debug, PartialEq, Eq)]
-#[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
-pub struct GetRoomEventResponse(pub EventInfo);
