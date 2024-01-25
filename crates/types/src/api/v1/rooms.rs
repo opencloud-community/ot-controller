ea27da6af909ffbda681af5b14fc55d0798896cc
Wolfgang Silbermayr <w.silbermayr@opentalk.eu>
2024-01-25 08:28:08 +0100

    refactor(types): move StartRoomError into separate module

--- crates/types/src/api/v1/rooms.rs
+++ crates/types/src/api/v1/rooms.rs
@@ -4,8 +4,6 @@
 
 //! This module contains types that are used for OpenTalk API V1 rooms endpoints.
 
-use strum::AsRefStr;
-
 use crate::{
     common::event::EventInfo,
     core::{BreakoutRoomId, ResumptionToken, RoomId},
@@ -19,6 +17,7 @@ mod post_rooms_request_body;
 mod post_rooms_start_request_body;
 mod room_resource;
 mod rooms_start_response;
+mod start_room_error;
 
 pub mod sip_config_resource;
 pub mod streaming_targets;
@@ -28,23 +27,7 @@ pub use post_rooms_request_body::PostRoomsRequestBody;
 pub use post_rooms_start_request_body::PostRoomsStartRequestBody;
 pub use room_resource::RoomResource;
 pub use rooms_start_response::RoomsStartResponse;
-
-/// Errors for the /rooms/{room_id}/start* endpoint
-#[derive(Clone, Debug, AsRefStr)]
-#[strum(serialize_all = "snake_case")]
-pub enum StartRoomError {
-    /// The provided room password is wrong
-    WrongRoomPassword,
-
-    /// The requested room has no breakout rooms enabled
-    NoBreakoutRooms,
-
-    /// The provided breakout room ID is invalid
-    InvalidBreakoutRoomId,
-
-    /// The user requesting to start the room is banned from the room
-    BannedFromRoom,
-}
+pub use start_room_error::StartRoomError;
 
 /// The JSON body expected when making a *POST /rooms/{room_id}/start_invited*
 #[derive(Clone, Debug)]
@@ -80,26 +63,3 @@ pub struct GetRoomEventRequest(pub RoomId);
 #[derive(Clone, Debug, PartialEq, Eq)]
 #[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
 pub struct GetRoomEventResponse(pub EventInfo);
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use pretty_assertions::assert_eq;
-
-    #[test]
-    fn to_string() {
-        assert_eq!(
-            StartRoomError::WrongRoomPassword.as_ref(),
-            "wrong_room_password"
-        );
-        assert_eq!(
-            StartRoomError::NoBreakoutRooms.as_ref(),
-            "no_breakout_rooms"
-        );
-        assert_eq!(
-            StartRoomError::InvalidBreakoutRoomId.as_ref(),
-            "invalid_breakout_room_id"
-        );
-        assert_eq!(StartRoomError::BannedFromRoom.as_ref(), "banned_from_room");
-    }
-}
