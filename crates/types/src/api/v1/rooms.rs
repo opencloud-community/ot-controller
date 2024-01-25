8489862a85cf0682d6b0cde78b9f02b77aca440d
Wolfgang Silbermayr <w.silbermayr@opentalk.eu>
2024-01-25 07:53:36 +0100

    refactor(types): move room resource into separate module

--- crates/types/src/api/v1/rooms.rs
+++ crates/types/src/api/v1/rooms.rs
@@ -4,7 +4,6 @@
 
 //! This module contains types that are used for OpenTalk API V1 rooms endpoints.
 
-use chrono::{DateTime, Utc};
 use strum::AsRefStr;
 
 use crate::{
@@ -15,33 +14,12 @@ use crate::{
 #[allow(unused_imports)]
 use crate::imports::*;
 
-use super::users::PublicUserProfile;
+mod room_resource;
 
 pub mod sip_config_resource;
 pub mod streaming_targets;
 
-/// A Room
-///
-/// Contains all room information. Is only be accessible to the owner and users with
-/// appropriate permissions.
-#[derive(Clone, Debug)]
-#[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
-pub struct RoomResource {
-    /// The ID of the room
-    pub id: RoomId,
-
-    /// The public user profile of the room's owner
-    pub created_by: PublicUserProfile,
-
-    /// The date when the room was created
-    pub created_at: DateTime<Utc>,
-
-    /// The password of the room, if any
-    pub password: Option<String>,
-
-    /// If waiting room is enabled
-    pub waiting_room: bool,
-}
+pub use room_resource::RoomResource;
 
 /// API request parameters to create a new room
 #[derive(Clone, Debug)]
