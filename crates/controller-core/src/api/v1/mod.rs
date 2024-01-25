99624ae89425b50a1f9489e893bebcdc7a446355
Wolfgang Silbermayr <w.silbermayr@opentalk.eu>
2024-01-25 07:24:47 +0100

    chore: remove api v1 module documentation which gets out of date easily

--- crates/controller-core/src/api/v1/mod.rs
+++ crates/controller-core/src/api/v1/mod.rs
@@ -2,22 +2,7 @@
 //
 // SPDX-License-Identifier: EUPL-1.2
 
-//! REST API v1
-//!
-//! Current Endpoints. See their respective function:
-//! - `/auth/login` ([GET](auth::get_login), [POST](auth::post_login))
-//! - `/rooms` ([GET](rooms::accessible), [POST](rooms::new))
-//! - `/rooms/{room_id}` ([GET](rooms::get), [PATCH](rooms::patch))
-//! - `/rooms/{room_id}/start` ([POST](rooms::start))
-//! - `/rooms/{room_id}/start_invited` ([POST](rooms::start_invited))
-//! - `/rooms/{room_id}/invites ([GET](invites::get_invites), [POST](invites::add_invite))
-//! - `/rooms/{room_id}/invites/{invite_code} ([GET](invites::get_invite), [PUT](invites::update_invite), [DELETE](invites::delete_invite)])
-//! - `/rooms/{room_id}/sip ([GET](sip_configs::get), [PUT](sip_configs::put), [DELETE](sip_configs::delete))
-//! - `/turn` ([GET](turn::get))
-//! - `/users/me`([GET](users::get_me), [PATCH](users::patch_me))
-//! - `/users/{user_id}` ([GET](users::get_user))
-//! - `/users/find` ([GET](users::find))
-//! - `/services/call_in/start ([POST](services::call_in::start))
+//! OpenTalk controller REST API v1 service handlers
 
 pub use response::{ApiResponse, DefaultApiResult};
 
