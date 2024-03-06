d2daae16c62f16a54dfcccc20946417c8c46b1a1
Wolfgang Silbermayr <w.silbermayr@opentalk.eu>
2024-03-06 16:49:19 +0100

    enclose generated docs as markdown instead of their raw format
    See #707

--- ci/docs-update-generated-parts.sh
+++ ci/docs-update-generated-parts.sh
@@ -21,36 +21,44 @@ cp extra/example.toml "$CONFIG_DIR"/example.toml
 
 cargo build --package "$OPENTALK_CONTROLLER_PROJECT"
 
-$OPENTALK_CONTROLLER_CMD help > "$CLI_DIR"/"$CMDNAME"-help
-$OPENTALK_CONTROLLER_CMD fix-acl --help > "$CLI_DIR"/"$CMDNAME"-fix-acl-help
-$OPENTALK_CONTROLLER_CMD acl --help > "$CLI_DIR"/"$CMDNAME"-acl-help
-$OPENTALK_CONTROLLER_CMD migrate-db --help > "$CLI_DIR"/"$CMDNAME"-migrate-db-help
-$OPENTALK_CONTROLLER_CMD tenants --help > "$CLI_DIR"/"$CMDNAME"-tenants-help
-$OPENTALK_CONTROLLER_CMD tenants list --help > "$CLI_DIR"/"$CMDNAME"-tenants-list-help
-$OPENTALK_CONTROLLER_CMD tenants set-oidc-id --help > "$CLI_DIR"/"$CMDNAME"-tenants-set-oidc-id-help
-$OPENTALK_CONTROLLER_CMD tariffs --help > "$CLI_DIR"/"$CMDNAME"-tariffs-help
-$OPENTALK_CONTROLLER_CMD tariffs create --help > "$CLI_DIR"/"$CMDNAME"-tariffs-create
-$OPENTALK_CONTROLLER_CMD tariffs delete --help > "$CLI_DIR"/"$CMDNAME"-tariffs-delete
-$OPENTALK_CONTROLLER_CMD tariffs edit --help > "$CLI_DIR"/"$CMDNAME"-tariffs-edit
-$OPENTALK_CONTROLLER_CMD jobs --help > "$CLI_DIR"/"$CMDNAME"-jobs-help
-$OPENTALK_CONTROLLER_CMD jobs execute --help > "$CLI_DIR"/"$CMDNAME"-jobs-execute-help
+codify() {
+  echo '```'"$1"
+  while read -r data; do
+    echo "$data"
+  done
+  echo '```'
+}
+
+$OPENTALK_CONTROLLER_CMD help | codify text > "$CLI_DIR"/"$CMDNAME"-help.md
+$OPENTALK_CONTROLLER_CMD fix-acl --help | codify text > "$CLI_DIR"/"$CMDNAME"-fix-acl-help.md
+$OPENTALK_CONTROLLER_CMD acl --help | codify text > "$CLI_DIR"/"$CMDNAME"-acl-help.md
+$OPENTALK_CONTROLLER_CMD migrate-db --help | codify text > "$CLI_DIR"/"$CMDNAME"-migrate-db-help.md
+$OPENTALK_CONTROLLER_CMD tenants --help | codify text > "$CLI_DIR"/"$CMDNAME"-tenants-help.md
+$OPENTALK_CONTROLLER_CMD tenants list --help | codify text > "$CLI_DIR"/"$CMDNAME"-tenants-list-help.md
+$OPENTALK_CONTROLLER_CMD tenants set-oidc-id --help | codify text > "$CLI_DIR"/"$CMDNAME"-tenants-set-oidc-id-help.md
+$OPENTALK_CONTROLLER_CMD tariffs --help | codify text > "$CLI_DIR"/"$CMDNAME"-tariffs-help.md
+$OPENTALK_CONTROLLER_CMD tariffs create --help | codify text > "$CLI_DIR"/"$CMDNAME"-tariffs-create.md
+$OPENTALK_CONTROLLER_CMD tariffs delete --help | codify text > "$CLI_DIR"/"$CMDNAME"-tariffs-delete.md
+$OPENTALK_CONTROLLER_CMD tariffs edit --help | codify text > "$CLI_DIR"/"$CMDNAME"-tariffs-edit.md
+$OPENTALK_CONTROLLER_CMD jobs --help | codify text > "$CLI_DIR"/"$CMDNAME"-jobs-help.md
+$OPENTALK_CONTROLLER_CMD jobs execute --help | codify text > "$CLI_DIR"/"$CMDNAME"-jobs-execute-help.md
 $OPENTALK_CONTROLLER_CMD \
   --config extra/example.toml \
   jobs \
   execute \
   self-check \
   --hide-duration \
-  > "$CLI_DIR"/"$CMDNAME"-jobs-execute-self-check
-$OPENTALK_CONTROLLER_CMD jobs default-parameters --help > "$CLI_DIR"/"$CMDNAME"-jobs-default-parameters-help
-$OPENTALK_CONTROLLER_CMD modules --help > "$CLI_DIR"/"$CMDNAME"-modules-help
-$OPENTALK_CONTROLLER_CMD modules list --help > "$CLI_DIR"/"$CMDNAME"-modules-list-help
+  | codify text > "$CLI_DIR"/"$CMDNAME"-jobs-execute-self-check.md
+$OPENTALK_CONTROLLER_CMD jobs default-parameters --help | codify text > "$CLI_DIR"/"$CMDNAME"-jobs-default-parameters-help.md
+$OPENTALK_CONTROLLER_CMD modules --help | codify text > "$CLI_DIR"/"$CMDNAME"-modules-help.md
+$OPENTALK_CONTROLLER_CMD modules list --help | codify text > "$CLI_DIR"/"$CMDNAME"-modules-list-help.md
 
-$OPENTALK_CONTROLLER_CMD --config extra/example.toml jobs default-parameters self-check > "$JOBS_DIR"/parameters-self-check.json
-$OPENTALK_CONTROLLER_CMD --config extra/example.toml jobs default-parameters event-cleanup > "$JOBS_DIR"/parameters-event-cleanup.json
-$OPENTALK_CONTROLLER_CMD --config extra/example.toml jobs default-parameters adhoc-event-cleanup > "$JOBS_DIR"/parameters-adhoc-event-cleanup.json
-$OPENTALK_CONTROLLER_CMD --config extra/example.toml jobs default-parameters invite-cleanup > "$JOBS_DIR"/parameters-invite-cleanup.json
+$OPENTALK_CONTROLLER_CMD --config extra/example.toml jobs default-parameters self-check | codify json > "$JOBS_DIR"/parameters-self-check.json.md
+$OPENTALK_CONTROLLER_CMD --config extra/example.toml jobs default-parameters event-cleanup | codify json > "$JOBS_DIR"/parameters-event-cleanup.json.md
+$OPENTALK_CONTROLLER_CMD --config extra/example.toml jobs default-parameters adhoc-event-cleanup | codify json > "$JOBS_DIR"/parameters-adhoc-event-cleanup.json.md
+$OPENTALK_CONTROLLER_CMD --config extra/example.toml jobs default-parameters invite-cleanup | codify json > "$JOBS_DIR"/parameters-invite-cleanup.json.md
 
-$OPENTALK_CONTROLLER_CMD --config extra/example.toml modules list > "$CLI_DIR"/"$CMDNAME"-modules-list
+$OPENTALK_CONTROLLER_CMD --config extra/example.toml modules list | codify text > "$CLI_DIR"/"$CMDNAME"-modules-list.md
 
 # Remove trailing spaces to prevent markdownlint from triggering *MD009 - Trailing spaces*
 # https://github.com/markdownlint/markdownlint/blob/main/docs/RULES.md#md009---trailing-spaces
