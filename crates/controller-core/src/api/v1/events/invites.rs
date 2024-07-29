279f2eb632b690a43006e8efc26ca8cfee3e859f
Wolfgang Silbermayr <w.silbermayr@opentalk.eu>
2024-07-29 17:00:57 +0200

    feat(api): expose `/events/{event_id}/invite` DELETE endpoint in openapi spec

--- crates/controller-core/src/api/v1/events/invites.rs
+++ crates/controller-core/src/api/v1/events/invites.rs
@@ -1180,9 +1180,39 @@ pub async fn accept_event_invite(
     Ok(NoContent)
 }
 
-/// API Endpoint `DELETE /events/{event_id}/invite`
-///
 /// Decline an invite to an event
+///
+/// No content required, the request will accept the invitation.
+#[utoipa::path(
+    params(
+        ("event_id" = EventId, description = "The id of the event"),
+    ),
+    responses(
+        (
+            status = StatusCode::NO_CONTENT,
+            description = "Invitation was declined",
+        ),
+        (
+            status = StatusCode::NOT_FOUND,
+            response = NotFound,
+        ),
+        (
+            status = StatusCode::UNAUTHORIZED,
+            response = Unauthorized,
+        ),
+        (
+            status = StatusCode::FORBIDDEN,
+            response = Forbidden,
+        ),
+        (
+            status = StatusCode::INTERNAL_SERVER_ERROR,
+            response = InternalServerError,
+        ),
+    ),
+    security(
+        ("BearerAuth" = []),
+    ),
+)]
 #[delete("/events/{event_id}/invite")]
 pub async fn decline_event_invite(
     db: Data<Db>,
