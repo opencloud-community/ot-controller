1aafa06724f98d44067e3692c6d0b9f96f9ad238
Wolfgang Silbermayr <w.silbermayr@opentalk.eu>
2024-09-25 06:44:53 +0200

    refactor(types): split opentalk-types-common utils module into multiple files
    changelog: ignore
    
    See #820, #870

---
+++ crates/types-common/src/utils/mod.rs
@@ -0,0 +1,14 @@
+// SPDX-FileCopyrightText: OpenTalk GmbH <mail@opentalk.eu>
+//
+// SPDX-License-Identifier: EUPL-1.2
+
+//! Helpful utilities used in this crate, but also useful outside of it.
+
+#[cfg(feature = "serde")]
+pub mod duration_seconds;
+#[cfg(feature = "serde")]
+pub mod duration_seconds_option;
+
+mod example_data;
+
+pub use example_data::ExampleData;
