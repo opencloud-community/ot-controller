b9d7260fd94ad21b723186c1a5acedad5694c396
Wolfgang Silbermayr <w.silbermayr@opentalk.eu>
2024-10-08 09:01:40 +0200

    feat(jobs): add user request page size in keycloak-account-sync job
    Closes #875

--- crates/jobs/src/jobs/keycloak_account_sync.rs
+++ crates/jobs/src/jobs/keycloak_account_sync.rs
@@ -10,28 +10,71 @@ use opentalk_controller_settings::Settings;
 use opentalk_database::Db;
 use opentalk_db_storage::users::User;
 use opentalk_keycloak_admin::KeycloakAdminClient;
-use opentalk_log::info;
+use opentalk_log::{debug, info};
 use opentalk_signaling_core::ExchangeHandle;
+use serde::{Deserialize, Serialize};
+use snafu::{ensure, ResultExt};
 
-use crate::{events::update_user_accounts, Error, Job};
+use crate::{
+    error::{
+        InvalidParameterValueSnafu, KeycloakApiReturnedInvalidUserCountSnafu,
+        ParameterLoadingSnafu, ParameterSerializingSnafu,
+    },
+    events::update_user_accounts,
+    Error, Job, JobParameters,
+};
 
-const KEYCLOAK_USER_LIST_LIMIT: i32 = 100;
+const DEFAULT_KEYCLOAK_API_PAGE_SIZE: usize = 100;
+const MIN_KEYCLOAK_API_PAGE_SIZE: usize = 1;
+const MAX_KEYCLOAK_API_PAGE_SIZE: usize = 10000;
 
 #[derive(Debug)]
 pub struct KeycloakAccountSync;
 
+#[derive(Debug, PartialEq, Eq, Serialize, Deserialize)]
+pub struct KeycloakAccountSyncParameters {
+    #[serde(default = "default_keycloak_api_page_size")]
+    keycloak_api_page_size: usize,
+}
+
+impl JobParameters for KeycloakAccountSyncParameters {
+    fn try_from_json(json: serde_json::Value) -> Result<Self, Error> {
+        serde_json::from_value(json).context(ParameterLoadingSnafu)
+    }
+
+    fn to_json(&self) -> Result<serde_json::Value, Error> {
+        serde_json::to_value(self).context(ParameterSerializingSnafu)
+    }
+}
+
 #[async_trait]
 impl Job for KeycloakAccountSync {
-    type Parameters = ();
+    type Parameters = KeycloakAccountSyncParameters;
 
     async fn execute(
         logger: &dyn Log,
         db: Arc<Db>,
         _exchange_handle: ExchangeHandle,
         settings: &Settings,
-        _parameters: Self::Parameters,
+        parameters: Self::Parameters,
     ) -> Result<(), Error> {
         info!(log: logger, "Starting account cleanup job");
+        debug!(log: logger, "Job parameters: {parameters:?}");
+
+        let KeycloakAccountSyncParameters {
+            keycloak_api_page_size,
+        } = parameters;
+
+        ensure!(
+            (MIN_KEYCLOAK_API_PAGE_SIZE..=MAX_KEYCLOAK_API_PAGE_SIZE)
+                .contains(&keycloak_api_page_size),
+            InvalidParameterValueSnafu {
+                parameter_name: "keycloak_api_page_size",
+                expected_requirement: format!(
+                    "Value between {MIN_KEYCLOAK_API_PAGE_SIZE} and {MAX_KEYCLOAK_API_PAGE_SIZE}"
+                ),
+            }
+        );
 
         let mut conn = db.get_conn().await?;
 
@@ -45,15 +88,30 @@ impl Job for KeycloakAccountSync {
         )?;
 
         let kc_users_count = kc_admin_client.get_users_count().await?;
+
+        let kc_users_count = usize::try_from(kc_users_count).with_context(|_| {
+            KeycloakApiReturnedInvalidUserCountSnafu {
+                returned_user_count: kc_users_count,
+            }
+        })?;
+
         let mut kc_users = vec![];
         let mut offset = 0;
 
         while offset < kc_users_count {
-            let chunk_size = std::cmp::min(KEYCLOAK_USER_LIST_LIMIT, kc_users_count - offset);
-            let users_chunk = kc_admin_client.get_users_chunk(offset, chunk_size).await?;
+            let chunk_size = std::cmp::min(keycloak_api_page_size, kc_users_count - offset);
+            debug!(log: logger, "Querying {chunk_size} users from Keycloak at offset {offset}.");
+            let users_chunk = kc_admin_client
+                .get_users_chunk(
+                    offset.try_into().expect("Valid i32 number required"),
+                    chunk_size.try_into().expect("Valid i32 number required"),
+                )
+                .await?;
             kc_users.extend(users_chunk);
             offset += chunk_size;
         }
+
+        debug!(log: logger, "Fetched {} users from Keycloak", kc_users.len());
 
         let user_attribute_name = settings.keycloak.external_id_user_attribute_name.as_deref();
 
@@ -61,4 +119,8 @@ impl Job for KeycloakAccountSync {
 
         Ok(())
     }
+}
+
+fn default_keycloak_api_page_size() -> usize {
+    DEFAULT_KEYCLOAK_API_PAGE_SIZE
 }
