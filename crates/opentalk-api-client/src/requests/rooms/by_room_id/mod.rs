1a9161c4eed2ca6f9e164fede3abc0e3bed62ba0
Wolfgang Silbermayr <w.silbermayr@opentalk.eu>
2024-11-26 17:36:27 +0100

    refactor(types): move rooms::PostRoomsStartInvitedRequest to opentalk-api-client
    changelog: ignore
    
    See #820, #881

--- crates/opentalk-api-client/src/requests/rooms/by_room_id/mod.rs
+++ crates/opentalk-api-client/src/requests/rooms/by_room_id/mod.rs
@@ -7,5 +7,7 @@
 pub mod invites;
 
 mod get_room_event_request;
+mod post_rooms_start_invited_request;
 
 pub use get_room_event_request::GetRoomEventRequest;
+pub use post_rooms_start_invited_request::PostRoomsStartInvitedRequest;
--- crates/opentalk-types/src/api/v1/rooms/mod.rs
+++ crates/opentalk-types/src/api/v1/rooms/mod.rs
@@ -4,11 +4,9 @@
 
 //! This module contains types that are used for OpenTalk API V1 rooms endpoints.
 
-mod post_rooms_start_invited_request;
 mod post_rooms_start_request;
 
 pub mod sip_config_resource;
 pub mod streaming_targets;
 
-pub use post_rooms_start_invited_request::PostRoomsStartInvitedRequest;
 pub use post_rooms_start_request::PostRoomsStartRequest;
