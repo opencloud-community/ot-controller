7a18b2e3fd68f1e61033aead8abf20fdc45eb04c
Sandro Forster <s.forster@opentalk.eu>
2025-02-11 15:54:13 +0100

    feat: implement e2ee signaling

--- crates/opentalk-community-signaling-modules/src/lib.rs
+++ crates/opentalk-community-signaling-modules/src/lib.rs
@@ -6,6 +6,7 @@ use async_trait::async_trait;
 use opentalk_signaling_core::{ModulesRegistrar, RegisterModules};
 use opentalk_signaling_module_chat::Chat;
 use opentalk_signaling_module_core::Core;
+use opentalk_signaling_module_e2ee::E2ee;
 use opentalk_signaling_module_integration::Integration;
 use opentalk_signaling_module_livekit::Livekit;
 use opentalk_signaling_module_meeting_notes::MeetingNotes;
@@ -24,6 +25,7 @@ impl RegisterModules for CommunityModules {
     async fn register<E>(registrar: &mut impl ModulesRegistrar<Error = E>) -> Result<(), E> {
         registrar.register::<Core>().await?;
         registrar.register::<Chat>().await?;
+        registrar.register::<E2ee>().await?;
         registrar.register::<Integration>().await?;
         registrar.register::<Livekit>().await?;
         registrar.register::<Polls>().await?;
