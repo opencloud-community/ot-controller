39ba8a74be7f820b7b99c4be76700c7b119faadc
Wolfgang Silbermayr <w.silbermayr@opentalk.eu>
2025-04-28 09:25:10 +0200

    fix(settings): properly load turn.lifetime field
    Closes #1039

--- crates/opentalk-controller-settings/src/settings_file/turn.rs
+++ crates/opentalk-controller-settings/src/settings_file/turn.rs
@@ -11,7 +11,11 @@ use super::TurnServer;
 #[derive(Debug, Clone, PartialEq, Eq, Deserialize)]
 pub(crate) struct Turn {
     /// How long a credential pair be should be valid, in seconds
-    #[serde(serialize_with = "opentalk_types_common::utils::duration_seconds_option")]
+    #[serde(
+        default,
+        with = "opentalk_types_common::utils::duration_seconds_option",
+        skip_serializing_if = "Option::is_none"
+    )]
     pub lifetime: Option<Duration>,
 
     /// List of configured TURN servers.
